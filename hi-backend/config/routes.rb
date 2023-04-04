d4bd892a793e1c7d621897f05a86bf5c8105aae4
ernest-phillips <ernest-phillips@github.com>
2023-04-04 10:41:05 -0700

    add items controller

--- hi-backend/config/routes.rb
+++ hi-backend/config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'items/index'
+  get 'items/show'
+  get 'items/create'
+  get 'items/edit'
+  get 'items/update'
+  get 'items/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
