c73138e8ea2b3ba47835c1b22013d32d60697d94
ernest-phillips <ernest-phillips@github.com>
2025-04-26 18:38:29 -0700

    setup api in controllers

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,2 +1,40 @@
-class Api::V1::ItemsController < ApplicationController
+module Api
+  module V1
+
+end
+module Api
+  module V1
+    class ItemsController < ApplicationController
+      before_action :set_residence
+
+      def index
+        items = @residence.items
+        render json: items
+      end
+
+      def show
+        item = @residence.items.find(params[:id])
+        render json: item
+      end
+
+      def create
+        item = @residence.items.new(item_params)
+        if item.save
+          render json: item, status: :created
+        else
+          render json: item.errors, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def set_residence
+        @residence = Residence.find(params[:residence_id])
+      end
+
+      def item_params
+        params.require(:item).permit(:name, :description, :value)
+      end
+    end
+  end
 end
--- app/controllers/api/v1/residences_controller.rb
+++ app/controllers/api/v1/residences_controller.rb
@@ -1,2 +1,30 @@
-class Api::V1::ResidencesController < ApplicationController
+module Api
+  module V1
+    class ResidencesController < ApplicationController
+      def index
+        residences = Residence.all
+        render json: residences
+      end
+
+      def show
+        residence = Residence.find(params[:id])
+        render json: residence
+      end
+
+      def create
+        residence = Residence.new(residence_params)
+        if residence.save
+          render json: residence, status: :created
+        else
+          render json: residence.errors, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def residence_params
+        params.require(:residence).permit(:name, :address, :city, :state, :zip_code)
+      end
+    end
+  end
 end
