8a19ceaf9ddafc6b7ad068cc372ed29d02dcc753
ernest-phillips <ernest-phillips@github.com>
2025-04-28 13:14:04 -0700

    Initial devise setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,13 @@
+# frozen_string_literal: true
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  Rails.application.routes.draw do
+  get 'home/index'
+  devise_for :users
     namespace :api do
       namespace :v1 do
         resources :residences do
           resources :items
-        end
       end
     end
   end
+  root to: 'home#index'
 end
