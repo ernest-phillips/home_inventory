3ca4d6f4a42bded21ce88cf912d4d0dd860f1185
ernest-phillips <ernest-phillips@github.com>
2025-04-27 20:16:11 -0700

    change ruby version

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_04_27_201418) do
+ActiveRecord::Schema[7.1].define(version: 2025_04_27_203942) do
   create_table "items", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -18,6 +18,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_04_27_201418) do
     t.bigint "residence_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["residence_id", "name"], name: "index_items_on_residence_id_and_name", unique: true
     t.index ["residence_id"], name: "index_items_on_residence_id"
   end
 
