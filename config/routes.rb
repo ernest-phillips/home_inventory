4d45dc210e9f5c7bab47c984c97a25f12906ad19
ernest-phillips <ernest-phillips@github.com>
2025-04-29 11:45:38 -0700

    Fix route for index

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 # frozen_string_literal: true
 Rails.application.routes.draw do
-  get 'home/index'
+  root to: 'home#index'
+
   devise_for :users
-    namespace :api do
-      namespace :v1 do
-        resources :residences do
-          resources :items
+
+  namespace :api do
+    namespace :v1 do
+      resources :residences do
+        resources :items
       end
     end
   end
-  root to: 'home#index'
 end
