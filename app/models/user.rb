32a6080bb20c68b82c44c404d2704585860a19f5
ernest-phillips <ernest-phillips@github.com>
2025-04-29 15:43:04 -0700

    update user model with full name

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :items, dependent: :destroy
   has_many :residences, dependent: :destroy
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_04_29_221758) do
+ActiveRecord::Schema[7.1].define(version: 2025_04_29_224009) do
   create_table "items", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -44,6 +44,8 @@ ActiveRecord::Schema[7.1].define(version: 2025_04_29_221758) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
